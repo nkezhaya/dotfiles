32d1a7abe9a1ed23aba30e29af2a6dd3eae0ea92
Nick Kezhaya <nick@whitepaperclip.com>
2025-04-10 09:15:21 -0500

    No need for VM commands

--- fish/.config/fish/functions/vhalt.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function vhalt --description 'alias vhalt=vagrant halt'
-	vagrant halt $argv;
-end
--- fish/.config/fish/functions/vrsync.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function vrsync --description 'alias vrsync=vagrant instant-rsync-auto'
-	vagrant instant-rsync-auto $argv;
-end
