a862b64366fa8cbd8584aaa543b6d0d7732a3bcd
Nick Kezhaya <20113+nkezhaya@users.noreply.github.com>
2025-07-14 10:43:24 -0500

    Do not need LuaSnip

--- vim/.config/nvim/lua/plugins/completion.lua
+++ vim/.config/nvim/lua/plugins/completion.lua
@@ -14,18 +14,34 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      -- Custom keymap to match your CoC behavior
       keymap = {
         preset = 'none',
+
         ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-        ['<C-e>'] = { 'hide' },
-        ['<C-n>'] = { 'select_next', 'fallback' },
-        ['<C-p>'] = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-e>'] = { 'hide', 'fallback' },
+
+        ['<Tab>'] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          'snippet_forward',
+          'fallback'
+        },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
         ['<Up>'] = { 'select_prev', 'fallback' },
-        
-        -- Enter accepts completion ONLY if an item is selected
-        ['<CR>'] = { 'accept', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
       },
 
       appearance = {
--- vim/.config/nvim/lua/plugins/snippets.lua
+++ vim/.config/nvim/lua/plugins/snippets.lua
@@ -1,20 +1,5 @@
 return {
   {
     "rafamadriz/friendly-snippets",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load({
-        paths = {
-          vim.fn.expand("snippets"),
-        }
-      })
-    end
   },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = { "rafamadriz/friendly-snippets" },
-    -- follow latest release.
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp",
-  }
 }
