ac8d3eec6b2ee8fa78795bc3cd80c6a9f41f1379
Nick Kezhaya <nick@whitepaperclip.com>
2025-07-14 09:14:47 -0500

    Trailing comma

--- vim/.config/nvim/lua/plugins/snippets.lua
+++ vim/.config/nvim/lua/plugins/snippets.lua
@@ -15,6 +15,6 @@ return {
     -- follow latest release.
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     -- install jsregexp (optional!).
-    build = "make install_jsregexp"
+    build = "make install_jsregexp",
   }
 }
