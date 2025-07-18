4a75191a97c31bb6c62e0e0576332e20b9c8d671
Nick Kezhaya <nick@whitepaperclip.com>
2025-07-18 07:34:41 -0500

    Removed projectionist

--- vim/.config/nvim/lua/plugins/editing.lua
+++ vim/.config/nvim/lua/plugins/editing.lua
@@ -1,26 +1,5 @@
 return {
   "tpope/vim-sensible",
   "tpope/vim-surround",
-  "tpope/vim-projectionist",
-  {
-    "mattn/emmet-vim",
-    config = function()
-      -- Emmet settings
-      vim.g.user_emmet_settings = {
-        ["javascript.jsx"] = {
-          extends = "jsx",
-        },
-      }
-      
-      vim.g.user_emmet_install_global = 0
-      
-      -- Enable emmet for specific filetypes
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "html", "css", "eelixir" },
-        callback = function()
-          vim.cmd("EmmetInstall")
-        end,
-      })
-    end,
-  },
+  "tpope/vim-projectionist"
 }
