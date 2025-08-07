041e483a1f553ef14b57944d288565c1bd9f108f
Nick Kezhaya <nick@whitepaperclip.com>
2025-08-07 10:48:43 -0500

    next/prev errors

--- vim/.config/nvim/lua/config/remap.lua
+++ vim/.config/nvim/lua/config/remap.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = ","
 
+-- go to prev/next error
+vim.keymap.set('n', '<leader>e', vim.diagnostic.goto_next, { noremap = true })
+vim.keymap.set('n', '<leader>E', vim.diagnostic.goto_prev, { noremap = true })
+
 vim.keymap.set('n', '<leader>|', ':vsplit<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>-', ':split<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>t', ':tab split<CR>', { noremap = true })
