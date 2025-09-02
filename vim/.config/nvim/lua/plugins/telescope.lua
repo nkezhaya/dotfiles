ef58d72d53e4f8592491de48d437c3fdc727d6c2
Nick Kezhaya <20113+nkezhaya@users.noreply.github.com>
2025-09-02 09:10:12 -0500

    Telescope escape, no leader prefix

--- vim/.config/nvim/lua/plugins/telescope.lua
+++ vim/.config/nvim/lua/plugins/telescope.lua
@@ -4,9 +4,18 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', 'ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', 'fg', builtin.live_grep, { desc = 'Telescope live grep' })
+
+    local actions = require("telescope.actions")
+    require("telescope").setup{
+      defaults = {
+        mappings = {
+          i = {
+            ["<esc>"] = actions.close
+          },
+        },
+      }
+    }
   end,
 }
