b9edca1d468c34e9aef8e8484ba6029072fb04c0
Nick Kezhaya <nick@whitepaperclip.com>
2025-09-08 13:19:57 -0500

    tmix, fmix

---
+++ fish/.config/fish/functions/fmix.fish
@@ -0,0 +1,4 @@
+function fmix --wraps='mix format' --description 'alias fmix=mix format'
+  mix format $argv
+        
+end
---
+++ fish/.config/fish/functions/tmix.fish
@@ -0,0 +1,4 @@
+function tmix --wraps='mix test' --description 'alias tmix=mix test'
+  mix test $argv
+        
+end
