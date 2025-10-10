c65a679f977aad27b17bebc3b17958a240fb2a3a
Nick Kezhaya <20113+nkezhaya@users.noreply.github.com>
2025-10-10 09:38:03 -0500

    Optional autoformat

--- vim/.config/nvim/lua/plugins/format.lua
+++ vim/.config/nvim/lua/plugins/format.lua
@@ -8,7 +8,14 @@ return {
         heex = { "mix" },
         surface = { "mix" },
       },
-      format_on_save = true,
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
+        return { timeout_ms = 30000, lsp_format = "fallback" }
+      end,
     })
   end,
 }
