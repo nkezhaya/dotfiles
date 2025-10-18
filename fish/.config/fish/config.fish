2335687f373b0f154f190a956fe294137501fb9e
Nick Kezhaya <nick@whitepaperclip.com>
2025-10-18 08:56:12 -0500

    Replaced asdf with mise

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,11 +23,5 @@ pyenv init - fish | source
 # Spicetify
 fish_add_path /Users/nkezhaya/.spicetify
 
-# asdf
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-set -gx --prepend PATH $_asdf_shims
-set --erase _asdf_shims
+# mise
+mise activate fish | source
