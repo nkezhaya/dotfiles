539b16746cb5bd0987b46710234e226d82f7d64b
Nick Kezhaya <nick@whitepaperclip.com>
2025-11-05 10:28:30 -0600

    Add optional local lua file

--- vim/.config/nvim/lua/config/init.lua
+++ vim/.config/nvim/lua/config/init.lua
@@ -2,3 +2,9 @@ require("config.commands")
 require("config.set")
 require("config.remap")
 require("config.lazy")
+
+local local_config = vim.fn.stdpath("config") .. "/lua/config/local.lua"
+
+if vim.fn.filereadable(local_config) == 1 then
+  require("config.local")
+end
