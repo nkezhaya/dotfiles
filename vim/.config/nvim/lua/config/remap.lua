485ad5fb0ceb809870e1cc434d2df49cfa40bbeb
Nick Kezhaya <nick.kezhaya@river.com>
2025-11-13 11:02:46 -0600

    cnext/cprev for quicklist

--- vim/.config/nvim/lua/config/remap.lua
+++ vim/.config/nvim/lua/config/remap.lua
@@ -8,6 +8,10 @@ vim.keymap.set('n', '<leader>|', ':vsplit<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>-', ':split<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>t', ':tab split<CR>', { noremap = true })
 
+-- cnext/cprev for quicklist
+vim.keymap.set('n', '<C-j>', ':cnext<CR>', { noremap = true })
+vim.keymap.set('n', '<C-k>', ':cprev<CR>', { noremap = true })
+
 -- While in visual mode, <C-r> to search and replace highlighted text
 vim.keymap.set('v', '<C-r>', '"hy:%s/<C-r>h//gc<left><left><left>', { noremap = true })
 
