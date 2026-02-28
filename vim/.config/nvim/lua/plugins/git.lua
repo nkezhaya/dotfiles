8e5b39536d21822b60b97e7c3e5e7f6084ca7834
Nick Kezhaya <nick.kezhaya@river.com>
2026-02-28 10:40:35 -0600

    Neogit

--- vim/.config/nvim/lua/plugins/git.lua
+++ vim/.config/nvim/lua/plugins/git.lua
@@ -1,14 +1,15 @@
 return {
   {
-    "tpope/vim-fugitive",
-    config = function()
-      vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    end,
-  },
-  {
-    "sindrets/diffview.nvim",
+    "NeogitOrg/neogit",
+    lazy = true,
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
     },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
   },
 }
